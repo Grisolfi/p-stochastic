47c48ec586e572e70def5301247bde880e9c5dc1
Grisolfi <thiago.g1994@aluno.ifsc.edu.br>
2018-09-24 10:57:33 -0300

    rand_binom implementation

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # p-stochastic
 a repository for stochastic processes studies
+
+# how to execute
+
+1. `pip3 install -r requirements.txt`
+
+- `apt-get install python3-tk`
+
+- `python3 pre.py N` where N is the number os samples
--- pre.py
+++ pre.py
@@ -1,16 +1,49 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
+import sys
 
 class Estocasticos(object):
 
     def __init__(self):
         self.rand = np.random.random
         self.factorial = np.math.factorial
-        pass
 
     def rand_binom(self, n, p):
         return np.sum(self.rand(n)<p)
 
     def coef_binom(self, k, n):
         return self.factorial(n)/(self.factorial(k))*self.factorial(n-k)
+
+    def pmf_teorica(self, n, p):
+        kteorico = np.zeros(n)
+        l = np.arange(n)
+        for k in l:
+            kteorico[k] = self.coef_binom(k,n) * ((p**k)*(1-p)**(n-k))
+        return kteorico
+
+    def pmf_pratica(self, N, n, p):
+        kpratico = np.zeros(N)
+        l = np.arange(N)
+        for k in l:
+            kpratico[k] = pre.rand_binom(n,p)
+        return np.histogram(kpratico, np.arange(n))/N
+
+if __name__ == '__main__':
+
+    pre =  Estocasticos()
+    # PMF TEORICA
+    try:
+        N = int(sys.argv[1])
+        n = int(sys.argv[2])
+        p = float(sys.argv[3])
+
+        y = np.arange(n)
+
+        print(pre.pmf_teorica(n,p))
+        # plt.title("PMFs Binomiais")
+        # plt.stem(y, pre.pmf_teorica(n, p), 'b')
+        # # plt.stem(y, pre.pmf_pratica(N, n, p), 'r')
+        # plt.show()
+
+    except Exception as e:
+        print(e)
